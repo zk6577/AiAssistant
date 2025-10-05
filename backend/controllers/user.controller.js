5a7c99d6224ae9bf560a2757fe30b56f6269a66e
zk657 <zk5454721@gmail.com>
2025-10-05 23:34:53 +0530

    new changess

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,7 +2,7 @@
 // import geminiResponse from "../gemini.js";
 // import User from "../models/user.model.js";
 // import uploadOnCloudinary from "../utils/cloudinary.js";
-// import moment from "moment";
+import moment from "moment";
 
 // export const getCurrentUser=async (req,res)=>{
 // try{
@@ -125,6 +125,40 @@ import uploadOnCloudinary from "../utils/cloudinary.js";
 import multer from "multer";
 const upload = multer(); // for parsing FormData text fields if no file
 
+
+
+export const getCurrentUser=async (req,res)=>{
+try{
+const userId=req.userId;
+const user= await User.findById(userId).select("-password")
+if(!user){
+    return res.status(404).json({message:"Unauthorized USer"});
+
+
+
+}
+
+    return res.status(200).json(user);
+
+
+}catch(error){
+    return res.status(500).json({message:"Error in the User Controller"});
+
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 export const updateAssistant = async (req, res) => {
   try {
     // Handle both FormData and JSON
@@ -159,3 +193,73 @@ export const updateAssistant = async (req, res) => {
     return res.status(500).json({ message: "Error updating assistant" });
   }
 };
+export const askToAssistant= async (req,res)=>{
+    try{
+        const {command}=req.body;
+    const user= await User.findById(req.userId);
+    user.history.push(command);
+    user.save()
+    const userName=user.name;
+    const assistantName=user.assistantName;
+    const result = await geminiResponse(command,assistantName,userName);
+
+     const jsonMatch=result.match(/{[\s\S]*}/);
+     if(!jsonMatch ){
+        return res.status(400).json({response:"sorry, I can't understand"})
+     }
+    const gemResult=JSON.parse(jsonMatch[0]);
+     const type=gemResult.type;
+     switch(type){
+        case 'get-date' :
+            return res.json({
+                type,
+                userInput:gemResult.userInput,
+                response:`Current date is ${moment().format("YYYY-MM-DD")}`
+            });
+
+            case 'get-time':
+                    return res.json({
+                type,
+                userInput:gemResult.userInput,
+                response:`Current time is ${moment().format("hh:mm A")}`
+            });
+
+             case 'get-day':
+                    return res.json({
+                type,
+                userInput:gemResult.userInput,
+                response:`Today  is ${moment().format("dddd")}`
+            });
+                case 'get-month':
+                    return res.json({
+                type,
+                userInput:gemResult.userInput,
+                response:`Today  is ${moment().format("MMMM")}`
+            });
+           case 'google-search':
+           case 'youtube-search':
+            case 'youtube-play':
+        case 'facebook-open':
+         case 'general':
+         case 'weather-show':
+        case 'calculator-open':
+            case 'instagram-open':
+                return res.json({
+                    type,
+                    userInput:gemResult.userInput,
+                    response:gemResult.response
+                });
+
+                default :
+                return  res.status(400).json({response:" I didn't understand that command"})
+
+
+     }
+   
+
+    }catch(error){
+         console.error("askToAssistant error:", error);
+                  return  res.status(500).json({response:" Ask assistant error"})
+
+    }
+}
