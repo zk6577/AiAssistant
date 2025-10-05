6b3000f4114e9135ab29ab8fa21857e806247fda
Zk456 <167811292+zk6577@users.noreply.github.com>
2025-10-05 18:09:29 +0530

    Update UserContext.jsx

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -5,7 +5,7 @@ import axios from  "axios";
 export const userDataContext=createContext();
 
 function UserContext({children}) {
-    const serverUrl="http://localhost:8000"
+    const serverUrl="https://aiassiatantbackend.onrender.com"
 
  const[userData,setUserData]=useState(null);
 const [frontendImage,setFrontendImage]=useState(null);
