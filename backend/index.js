97c1cf924776291bfbc0917323138df83c847dc7
Zk456 <167811292+zk6577@users.noreply.github.com>
2025-10-05 19:28:32 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from "./gemini.js";
 
 const app=express();
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://aiassistant-e55v.onrender.com",
     credentials:true
 }))
 const port=process.env.PORT || 5000
